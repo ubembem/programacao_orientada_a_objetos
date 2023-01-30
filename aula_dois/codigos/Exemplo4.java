a64d9fc05e09b139a68b9ea54e630e881a9f6edd
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-01-30 14:33:13 -0300

    Update Exemplo4.java

--- aula_dois/codigos/Exemplo4.java
+++ aula_dois/codigos/Exemplo4.java
@@ -3,7 +3,7 @@ public class Exemplo {
        int n1 = 15, n2 = 30, n3 =20;
        if(n1>n2 && n1>n3){
            System.out.println(n1);
-       }else if(n2>n1 && n2>n3){
+       }else if(n2>n3){
             System.out.println(n2);
         }else {
             System.out.println(n3);
