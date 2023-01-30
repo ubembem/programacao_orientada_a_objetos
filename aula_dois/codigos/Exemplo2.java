e2f8a29e581e4e0e1dc9ea3e8360f75c2c22a5df
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-01-30 14:11:51 -0300

    Create Exemplo2.java

---
+++ aula_dois/codigos/Exemplo2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exemplo2 {
+    public static void main(String[] args) {
+        Scanner sc;
+        sc = new Scanner(System.in);
+        String entrada = sc.next();
+
+        System.out.println(entrada);
+
+        int num = 0;
+        num = sc.nextInt();
+
+        System.out.println(num);
+    }
+}
