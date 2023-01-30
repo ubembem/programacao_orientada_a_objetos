a10a2a13287fdf7f825b8c7c42b1c0bdce6d57fa
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-01-30 14:41:14 -0300

    Update Exemplo4.java

--- aula_dois/codigos/Exemplo4.java
+++ aula_dois/codigos/Exemplo4.java
@@ -1,12 +1,19 @@
-public class Exemplo {
+public class Exemplo4 {
     public static void main(String[] args) {
-       int n1 = 15, n2 = 30, n3 =20;
-       if(n1>n2 && n1>n3){
-           System.out.println(n1);
-       }else if(n2>n3){
-            System.out.println(n2);
+        int n1 = 0, n2 = 0, n3 = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite o primeiro valor");
+        n1 = sc.nextInt();
+        System.out.println("digite o segundo valor");
+        n2 = sc.nextInt();
+        System.out.println("digite o terceiro valor");
+        n3 = sc.nextInt();
+        if(n1>n2 && n1>n3){
+            System.out.printf("maior: %d",n1);
+        }else if(n2>n3){
+            System.out.printf("maior: %d",n2);
         }else {
-            System.out.println(n3);
+            System.out.printf("maior: %d",n3);
         }
     }
 }
