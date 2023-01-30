94a812c39c6741b7d153789009b48f91aa81e831
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-01-30 14:04:40 -0300

    Create Exemplo1.java

---
+++ aula_dois/codigos/Exemplo1.java
@@ -0,0 +1,12 @@
+public class Exemplo1 {
+    public static void main(String[] args) {
+        int n1 = 10;
+        float n2 = 5.4f;
+        double n3 = 5.4;
+        boolean n4 = false;
+        char n5 = 66; //0 a 255
+        String n6 = "frase";
+
+        System.out.println(n5);
+    }
+}
