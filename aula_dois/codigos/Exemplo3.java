d942bbcd0a19a8f85d939eddaaa3248f89b42198
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-01-30 14:31:27 -0300

    Create Exemplo3.java

---
+++ aula_dois/codigos/Exemplo3.java
@@ -0,0 +1,14 @@
+public class Exemplo {
+    public static void main(String[] args) {
+       int n1 = 15, n2 = 30, n3 =20;
+       if(n1>n2 && n1>n3){
+           System.out.println(n1);
+       }
+        if(n2>n1 && n2>n3){
+            System.out.println(n2);
+        }
+        if(n3>n1 && n3>n2){
+            System.out.println(n3);
+        }
+    }
+}
