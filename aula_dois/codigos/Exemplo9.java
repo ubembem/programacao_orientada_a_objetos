585f2dc2c13b7984e83e8c5c59d2d0499d3ae3fb
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-02 11:48:05 -0300

    Create Exemplo9.java

---
+++ aula_dois/codigos/Exemplo9.java
@@ -0,0 +1,23 @@
+public class Exemplo9 {
+    public static void main(String[] args) {
+        int m[][] = new int[2][3];
+        m[0][2] = 10;
+        m[2][1] = 50;
+
+        char c[][] = new char[2][4];
+        c[0][2] = 'a';
+        c[1][3] = 'b';
+
+        int m1[][] = new int[4][2];
+        int valor = 100;
+        for(int i=0;i<4;i++){
+            for(int z=0;z<2;z++){
+                m[i][z] = valor;
+                valor+=4;
+                System.out.printf("%d-%d: %d",i,z,m[i][z]);
+            }//for
+            System.out.println();
+        }//for
+
+    }//main
+}//class
