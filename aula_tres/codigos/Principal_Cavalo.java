e9853eb1083aa34d2755afc225d4e7e1e818d28e
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-09 12:57:23 -0300

    Create Principal_Cavalo.java

---
+++ aula_tres/codigos/Principal_Cavalo.java
@@ -0,0 +1,5 @@
+public class Principal {
+   public static void main(String[] args) {
+       Cavalo c1 = new Cavalo("Pé de Pano", 100, 5);  //o objeto do tipo Cavalo é criado já com valores atribuídos.
+   }
+}//class
