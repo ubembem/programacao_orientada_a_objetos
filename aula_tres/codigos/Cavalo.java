d4d1f1ca2269e01e4207bfa6e9253c61451284c3
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-09 12:56:59 -0300

    Create Cavalo.java

---
+++ aula_tres/codigos/Cavalo.java
@@ -0,0 +1,12 @@
+public class Cavalo {
+   String nome;
+   float peso;
+   int idade;
+
+   public Cavalo(String nome, float peso, int idade) {   //construtor 
+       this.nome = nome;
+       this.peso = peso;
+       this.idade = idade;
+   }//construtor
+}//class
+
