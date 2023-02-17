747b72031ebdc24ee0ed0d1732752fa4a28a07eb
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-17 15:31:09 -0300

    Create Gerente.java

---
+++ aula_seis/um/Gerente.java
@@ -0,0 +1,6 @@
+public class Gerente extends Pessoa{
+
+    public Gerente(String nome, int anoNascimento, double valorHora, double qtdHora) {
+        super(nome, anoNascimento, valorHora, qtdHora);
+    }
+}
