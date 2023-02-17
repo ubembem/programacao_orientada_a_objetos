0177ad073651059937b911609c9a53b3b9405c18
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-17 16:51:44 -0300

    Create Principal.java

---
+++ aula_sete/quatro/Principal.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.List;
+
+
+public class Principal {
+
+   public static void main(String[] args) {
+
+       List<Integer> dados = new ArrayList();
+
+       dados.add(40);
+
+       dados.add(25);
+
+       dados.add(30);
+
+       System.out.println(dados);
+
+       Collections.sort(dados);
+
+       System.out.println(dados);
+
+   }
+
+}
