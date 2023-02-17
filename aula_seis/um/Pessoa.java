3437f4c4cc0733ee29eb8271f2cb5ca6c94493da
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-17 15:30:30 -0300

    Create Pessoa.java

---
+++ aula_seis/um/Pessoa.java
@@ -0,0 +1,65 @@
+public class Pessoa {
+    protected String nome;
+    protected int anoNascimento;
+    protected double valorHora,qtdHora;
+
+    public Pessoa(String nome, int anoNascimento, double valorHora, double qtdHora) {
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.valorHora = valorHora;
+        this.qtdHora = qtdHora;
+    }
+
+    public Pessoa() {
+    }
+
+    public int obterIdade(int anoAtual){
+        return anoAtual-anoNascimento;
+    }
+
+    public double calcularSalarioMensal(){
+        return valorHora*qtdHora*4;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public double getQtdHora() {
+        return qtdHora;
+    }
+
+    public void setQtdHora(double qtdHora) {
+        this.qtdHora = qtdHora;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", anoNascimento=" + anoNascimento +
+                ", valorHora=" + valorHora +
+                ", qtdHora=" + qtdHora +
+                '}';
+    }
+}
