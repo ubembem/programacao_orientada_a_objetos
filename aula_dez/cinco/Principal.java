1a6f777889cdc153058390feeacf0ecda7c9eacd
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-22 17:30:52 -0300

    Create Principal.java

---
+++ aula_dez/cinco/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+
+   public static void main(String[] args) {
+
+       Pessoa p = new Pessoa("John",30);
+
+       EscritaObjeto.escrever("arquivoObj.txt",p);
+
+
+   }
+
+}
