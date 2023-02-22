a8da6387b97c47e6fd1cce5f6900f1ef43f98941
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-22 17:10:12 -0300

    Create Principal.java

---
+++ aula_dez/dois/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+
+   private static final String CAMINHO = "arquivo.txt";
+
+   public static void main(String[] args) {
+
+       LeituraArquivo.ler(CAMINHO);
+
+   }
+
+}//class
