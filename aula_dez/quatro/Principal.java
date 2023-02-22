d365b5f6b40806d98f3f96105dd083bd813dd307
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-22 17:18:30 -0300

    Create Principal.java

---
+++ aula_dez/quatro/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+
+   public static void main(String[] args) {
+
+       //EscritaBinario.escrever("arquivoBin.txt","Ana",1,3.75);
+
+       LeituraBinario.ler("arquivoBin.txt");
+
+   }
+
+}
