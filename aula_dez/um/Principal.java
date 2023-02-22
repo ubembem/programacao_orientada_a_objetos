067d3dafb436eb6d19a4bf49f2961b15492e087a
apag-guimaraes <99103993+apag-guimaraes@users.noreply.github.com>
2023-02-22 17:08:54 -0300

    Create Principal.java

---
+++ aula_dez/um/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+
+   private static final String CAMINHO = "arquivo.txt";
+
+   private static final String CONTEUDO = "teste";
+
+
+   public static void main(String[] args) {
+
+       EscritaTexto.escrever(CAMINHO,CONTEUDO);
+
+
+   }
+
+}//class
